3f3918b4b16ac1da61db877ffe2c39b52386c0e4
Jeedan <Jeedan.Cuneyt@gmail.com>
2026-03-13 22:50:37 +0300

    started on aggregating fetched feeds. Added mark feed fetched function which updates a new column. Added last_fetched-at column to feeds to be able to sort by oldest fetched feed

--- src/lib/db/queries/feeds.ts
+++ src/lib/db/queries/feeds.ts
@@ -28,6 +28,16 @@ export async function getFeedsWithUser() {
 
 export async function getFeedByUrl(url: string) {
 	const [result] = await db.select().from(feeds).where(eq(feeds.url, url));
+	if (!result) throw new Error(`Could not find feed with this url: ${url}`);
 
 	return result;
 }
+
+export async function markFeedFetched(feedId: string) {
+	await db
+		.update(feeds)
+		.set({ lastFetchedAt: new Date() })
+		.where(eq(feeds.id, feedId));
+}
+
+export async function getNextFeedToFetch() {}
